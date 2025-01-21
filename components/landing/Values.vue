2cda2cb4245ac06f750817074846869f37f39fc4
TekfolioNG <tekfoilong@gmail.com>
2025-01-21 10:09:29 +0100

    Mobile viewport issues on hero section fixed”

--- components/landing/Values.vue
+++ components/landing/Values.vue
@@ -1,16 +1,17 @@
 <template>
-    <section
-        class="relative bg-gradient-to-r from-[#02194F] to-[#01133A] w-screen py-12 sm:py-16 mt-[600px] sm:mt-[-75px] translate-x-[-5%] sm:translate-x-[-2.8%]">
+    <section class="relative bg-gradient-to-r from-[#02194F] to-[#01133A] w-screen py-12 sm:py-16 
+            mt-[60vh] xs:mt-[65vh] sm:mt-[-75px] 
+            translate-x-[-5%] sm:translate-x-[-2.8%]">
         <div class="container mx-auto px-4 sm:px-8">
             <div class="max-w-5xl mx-auto">
                 <p
                     class="text-white text-base sm:text-lg md:text-2xl leading-relaxed font-light text-center mx-auto md:max-w-3xl">
                     Oil and gas keep our world moving. That's why at <b><i>Sefa Energy</i></b>, we work hard to provide
                     the best solutions, from supplying quality products to managing complex projects. Our skilled teams
-                    and
-                    proven methods help businesses and individuals work and commute safely and protect their assets.
+                    and proven methods help businesses and individuals work and commute safely and protect their assets.
                 </p>
             </div>
         </div>
     </section>
 </template>
+
