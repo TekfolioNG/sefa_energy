7a40b9723f5b9de1cfa0dd8e4d9eed15b7284da9
TekfolioNG <tekfoilong@gmail.com>
2025-02-01 11:46:39 +0100

    Slight mobile view adjustment”

--- components/landing/Features.vue
+++ components/landing/Features.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative w-full py-8 sm:py-12 lg:py-16  mt-[74vh] xs:mt-[85vh] md:mt-[85vh] lg:mt-[82vh] xl:mt-[64vh]">
+  <div class="relative w-full py-8 sm:py-12 lg:py-16  mt-[72vh] xs:mt-[85vh] md:mt-[85vh] lg:mt-[82vh] xl:mt-[64vh]">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="relative z-10 text-center">
         <h2 class="text-3xl sm:text-4xl lg:text-5xl font-extrabold tracking-tight blue-filter">
